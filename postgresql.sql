ae53c59b8146f9eba62b57edd5b1553f6802e326
Andrew Kane <acekane1@gmail.com>
2013-08-02 23:51:34 -0700

    Cleaner format

--- postgresql.sql
+++ postgresql.sql
@@ -4,15 +4,19 @@
 CREATE OR REPLACE FUNCTION array_median(numeric[])
   RETURNS numeric AS
 $$
-    SELECT CASE
-    WHEN array_upper($1,1) = 0 THEN null
-    WHEN mod(array_upper($1,1),2) = 1 THEN asorted[ceiling(array_upper(asorted,1)/2.0)]
-    ELSE ((asorted[ceiling(array_upper(asorted,1)/2.0)] + asorted[ceiling(array_upper(asorted,1)/2.0)+1])/2.0) END
-    FROM (SELECT ARRAY(SELECT ($1)[n] FROM
-generate_series(1, array_upper($1, 1)) AS n
-    WHERE ($1)[n] IS NOT NULL
-            ORDER BY ($1)[n]
-) As asorted) As foo ;
+  SELECT CASE
+    WHEN array_upper($1, 1) = 0 THEN null
+    WHEN mod(array_upper($1, 1), 2) = 1 THEN asorted[ceiling(array_upper(asorted, 1) / 2.0)]
+    ELSE (
+      (asorted[ceiling(array_upper(asorted, 1) / 2.0)] + asorted[ceiling(array_upper(asorted, 1) / 2.0) + 1]) / 2.0
+    )
+    END
+  FROM (
+    SELECT ARRAY(
+      SELECT ($1)[n] FROM generate_series(1, array_upper($1, 1)) AS n
+      WHERE ($1)[n] IS NOT NULL ORDER BY ($1)[n]
+    ) AS asorted
+  ) AS foo;
 $$
   LANGUAGE SQL;
 
